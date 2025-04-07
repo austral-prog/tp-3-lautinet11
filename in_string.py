42e758f7863c78da6e2bf865d6f8d9fdccb8b367
lautaro Cusinato <76267353+lautinet11@users.noreply.github.com>
2025-04-07 09:29:06 -0400

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,3 +1,74 @@
+def check_vowels():
+    Nombre = "Matias"
+    a = "a" in Nombre or "A" in Nombre
+    a = str(a)
+    e = "e" in Nombre or "E" in Nombre
+    e = str(e)
+    i = "i" in Nombre or "I" in Nombre
+    i = str(i)
+    o = "o" in Nombre or "O" in Nombre
+    o = str(o)
+    u = "u" in Nombre or "U" in Nombre
+    u = str(u)
+    print("Contiene a: "+a)
+    print("Contiene e: "+e)
+    print("Contiene i: "+i)
+    print("Contiene o: "+o)
+    print("Contiene u: "+u)
+    Nombre = "Augusto"
+    a = "a" in Nombre or "A" in Nombre
+    a = str(a)
+    e = "e" in Nombre or "E" in Nombre
+    e = str(e)
+    i = "i" in Nombre or "I" in Nombre
+    i = str(i)
+    o = "o" in Nombre or "O" in Nombre
+    o = str(o)
+    u = "u" in Nombre or "U" in Nombre
+    u = str(u)
+    print("Contiene a: "+a)
+    print("Contiene e: "+e)
+    print("Contiene i: "+i)
+    print("Contiene o: "+o)
+    print("Contiene u: "+u)
+    Nombre = "Ines"
+    a = "a" in Nombre or "A" in Nombre
+    a = str(a)
+    e = "e" in Nombre or "E" in Nombre
+    e = str(e)
+    i = "i" in Nombre or "I" in Nombre
+    i = str(i)
+    o = "o" in Nombre or "O" in Nombre
+    o = str(o)
+    u = "u" in Nombre or "U" in Nombre
+    u = str(u)
+    print("Contiene a: "+a)
+    print("Contiene e: "+e)
+    print("Contiene i: "+i)
+    print("Contiene o: "+o)
+    print("Contiene u: "+u)
+    Nombre = "Juan"
+    a = "a" in Nombre or "A" in Nombre
+    a = str(a)
+    e = "e" in Nombre or "E" in Nombre
+    e = str(e)
+    i = "i" in Nombre or "I" in Nombre
+    i = str(i)
+    o = "o" in Nombre or "O" in Nombre
+    o = str(o)
+    u = "u" in Nombre or "U" in Nombre
+    u = str(u)
+    print("Contiene a: "+a)
+    print("Contiene e: "+e)
+    print("Contiene i: "+i)
+    print("Contiene o: "+o)
+    print("Contiene u: "+u)
+    
+check_vowels()
+
+
+# Lo habia hecho asi pero el corrector no funciona como esta en la consigna asi que me quedo todo largo !
+"""
 abc = 0
 while abc < 4:
     def check_vowels():
@@ -20,3 +91,4 @@ while abc < 4:
         print("Contiene u: "+u)
     abc = abc +1
     check_vowels()
+"""
